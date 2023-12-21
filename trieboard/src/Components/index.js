23a0647323afab67d9964c48b3e698693f29be01
Hyoboem Kim <hyoboemkim1@gmail.com>
2023-12-21 16:49:06 -0500

    Added zoom functionality

--- trieboard/src/Components/index.js
+++ trieboard/src/Components/index.js
@@ -1,16 +1,19 @@
 import React, {Component, useEffect, useRef, useState} from "react";
 import * as d3 from "d3";
 
-const BarChart = () => {
-    var margin = {top: 10, right: 5, bottom: 10, left: 100},
-        width = 1400 - margin.left - margin.right,
-        height = 900-margin.top - margin.bottom;
+const ForceGraph = () => {
+    var margin = {top: 0, right: 5, bottom: 40, left: 100},
+        width = window.innerWidth - margin.left - margin.right,
+        height = window.innerHeight -margin.top - margin.bottom,
+        node_radius = 40,
+        text_size = "1em";
+
 
     const [counter, setCounter] = useState(4);
     const [data, setData] = useState(
         {
-            "nodes": [{"id": 1, "group": 1}, {"id": 2, "group": 1}, {"id": 3, "group": 1}],
-            "links": [{"source": 2, "target": 1}, {"source": 3, "target": 1}]
+            "nodes": [{"id": "TrieBoard", "group": 1}],
+            "links": []
         }
         );
 
@@ -23,17 +26,32 @@ const BarChart = () => {
         const nodes = data.nodes;
         const links = data.links;
 
-        let node = svg.selectAll("circle")
+        svg.selectAll("g").remove()
+
+        let nodeTextGroup = svg.selectAll("g")
         .data(nodes);
         
-        node.exit()
+        nodeTextGroup.exit()
         .remove();
 
-        node = node.enter()
+        nodeTextGroup = nodeTextGroup.enter()
+        .append("g");
+
+        // nodeTextGroup = enteredNodeTextGroup.merge(nodeTextGroup);
+
+        // add the circle
+        const node = nodeTextGroup
         .append("circle")
-        .attr("r", 20)
-        .style("fill", "#69b3a2")
-        .merge(node);
+        .attr("r", node_radius)
+        .style("fill", "#69b3a2");
+        // .attr("transform", d => 'translate("+[width/2,height/2]+")');
+        
+        // add the text
+        const nodeText = nodeTextGroup
+        .append("text")
+        .text(d => d.id)
+        .attr("font-size", text_size)
+        .style("text-anchor", "middle");
 
         node.append("title").text(d => d.id);
 
@@ -50,7 +68,7 @@ const BarChart = () => {
         .attr("stroke-opacity", 0.6)
         .merge(link);
 
-        node.call(d3.drag()
+        nodeTextGroup.call(d3.drag()
         .on("start", dragstarted)
         .on("drag", dragged)
         .on("end", dragended));
@@ -64,15 +82,31 @@ const BarChart = () => {
 
 
         function ticked() {
+            // Allow user to move around nodes wherever they want 
+            // simulation
+            // .force("center", d3.forceCenter().strength(0))
+
             link
                 .attr("x1", d => d.source.x)
                 .attr("y1", d => d.source.y)
                 .attr("x2", d => d.target.x)
                 .attr("y2", d => d.target.y);
         
+            // nodeTextGroup
+            // .attr("transform", d => 'translate("+[d.x,d.y]+")');
+
             node
-                .attr("cx", d => d.x)
-                .attr("cy", d => d.y);
+            .attr("cx", d => d.x)
+            .attr("cy", d => d.y);
+
+            nodeText
+            .attr("x", d => d.x)
+            .attr("y", d => d.y);
+                // .attr("cx", d => d.x)
+                // .attr("cy", d => d.y);
+
+            // nodeTextGroup.attr('transform', (d) => `translate(${d.x},${d.y})`);
+            // link.attr('x1', (d) => d.source.x).attr('y1', (d) => d.source.y).attr('x2', (d) => d.target.x).attr('y2', (d) => d.target.y)
         };
 
         function dragstarted(event) {
@@ -93,14 +127,45 @@ const BarChart = () => {
             event.subject.fx = null;
             event.subject.fy = null;
         };
+        simulation.alpha(0.05).restart();
+
+
+        const zoomHandler = d3.zoom().on('zoom', (event) => {
+            // Apply transformations to the entire graph (nodes, links, etc.)
+            nodeTextGroup.attr('transform', event.transform);
+            link.attr('transform', event.transform);
+          });
+        
+        // Apply the zoom behavior to the SVG
+        svg.call(zoomHandler);
+    
+        // Initial zoom level (optional)
+        svg.call(zoomHandler.transform, d3.zoomIdentity.scale(1));
+
         
     }, [data]);
+
+    function addNode(e) {
+        const letters = /^[A-Za-z]+$/;
+        if (e.key.match(letters)) {
+            setData(
+                {
+                    "nodes": [...data.nodes, {"id": e.key, "group": 1}], 
+                    "links": [...data.links, {"source": e.key, "target": "TrieBoard"}]}
+                ); 
+            setCounter(counter + 1);
+        } else {
+            return;
+        }
+    }
     
 
     return <div>
         <svg ref={ref}/>
-        <input type="text" onKeyUp={(e) => {setData({"nodes": [...data.nodes, {"id": e.key, "group": 1}], "links": [...data.links, {"source": e.key, "target": 1}]}); setCounter(counter + 1)}}/>
-        </div>
+        <span>
+        <input type="text" onKeyUp={(e) => addNode(e)}/>
+        </span>
+    </div>
 }
 
-export default BarChart
+export default ForceGraph
