3f3be47e8803085cba60967bb7e8856808e52ba8
Hyoboem Kim <hyoboemkim1@gmail.com>
2023-12-23 23:07:27 -0500

    Small change to preserve zoom level even while typing new words

--- trieboard/src/Components/ForceGraph/index.js
+++ trieboard/src/Components/ForceGraph/index.js
@@ -10,8 +10,6 @@ const ForceGraph = () => {
         node_radius = 40,
         text_size = "1em";
 
-
-    const prefixTree = new Trie();
     const [data, setData] = useState(
         {
             "nodes": [{"id": "", "text": "TrieBoard", "group": ""}],
@@ -20,28 +18,25 @@ const ForceGraph = () => {
         );
 
     const [trie, setTrie] = useState(new Trie());
-
     const ref = useRef();
-    
 
     useEffect(() => {
-        const svg = d3.select(ref.current).attr("width", width).attr("height", height);
-
         const nodes = data.nodes;
         const links = data.links;
 
-        svg.selectAll("g").remove()
+        const svg = d3.select(ref.current).attr("width", width).attr("height", height);
 
-        let nodeTextGroup = svg.selectAll("g")
-        .data(nodes);
+        // Keep a copy of the previous zoom state to preserve zoom level
+        const prevZoom = d3.zoomTransform(svg.node());
+
+        svg.selectAll("g").remove();
+        let nodeTextGroup = svg.selectAll("g").data(nodes);
         
         nodeTextGroup.exit()
         .remove();
 
         nodeTextGroup = nodeTextGroup.enter()
         .append("g");
-
-        // nodeTextGroup = enteredNodeTextGroup.merge(nodeTextGroup);
 
         // add the circle
         const node = nodeTextGroup
@@ -71,18 +66,12 @@ const ForceGraph = () => {
         .attr("stroke-opacity", 0.6)
         .merge(link);
 
-        nodeTextGroup.call(d3.drag()
-        .on("start", dragstarted)
-        .on("drag", dragged)
-        .on("end", dragended));
-
-        const simulation = d3.forceSimulation(nodes)
+        const simulation = d3.forceSimulation(nodes)  // S3 simulation to apply forces between nodes in the graph 
         .force("link", d3.forceLink(links).id(d => d.id))
         .force("charge", d3.forceManyBody().strength(-200))
         .force("center", d3.forceCenter(width / 2, height / 2))
         .force("collide", d3.forceCollide(100))
-        .on("tick", ticked);
-
+        .on("tick", ticked); 
 
         function ticked() {
             // Allow user to move around nodes wherever they want 
@@ -94,9 +83,6 @@ const ForceGraph = () => {
                 .attr("y1", d => d.source.y)
                 .attr("x2", d => d.target.x)
                 .attr("y2", d => d.target.y);
-        
-            // nodeTextGroup
-            // .attr("transform", d => 'translate("+[d.x,d.y]+")');
 
             node
             .attr("cx", d => d.x)
@@ -106,6 +92,11 @@ const ForceGraph = () => {
             .attr("x", d => d.x)
             .attr("y", d => d.y);
         };
+
+        nodeTextGroup.call(d3.drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended));
 
         function dragstarted(event) {
             if (!event.active) simulation.alphaTarget(0.1).restart();
@@ -137,12 +128,126 @@ const ForceGraph = () => {
         
         // Apply the zoom behavior to the SVG
         svg.call(zoomHandler);
+        // nodeTextGroup.call(zoomHandler);
+        svg.call(zoomHandler.transform, prevZoom);
+
+    }, [data])
+    //     const svg = d3.select(ref.current).attr("width", width).attr("height", height);
+
+    //     const nodes = data.nodes;
+    //     const links = data.links;
+
+    //     svg.selectAll("g").remove()
+
+    //     let nodeTextGroup = svg.selectAll("g")
+    //     .data(nodes);
+        
+    //     nodeTextGroup.exit()
+    //     .remove();
+
+    //     nodeTextGroup = nodeTextGroup.enter()
+    //     .append("g");
+
+    //     // add the circle
+    //     const node = nodeTextGroup
+    //     .append("circle")
+    //     .attr("r", node_radius)
+    //     .style("fill", "#69b3a2");
+        
+    //     // add the text
+    //     const nodeText = nodeTextGroup
+    //     .append("text")
+    //     .text(d => d.text)
+    //     .attr("font-size", text_size)
+    //     .style("text-anchor", "middle");
+
+    //     node.append("title").text(d => d.id);
+
+    //     let link = svg.selectAll("line")
+    //     .data(links)
+        
+    //     link.exit()
+    //     .remove();
+
+    //     link = link
+    //     .enter()
+    //     .append("line")
+    //     .attr("stroke", "#999")
+    //     .attr("stroke-opacity", 0.6)
+    //     .merge(link);
+
+    //     // nodeTextGroup.call(d3.drag()
+    //     // .on("start", dragstarted)
+    //     // .on("drag", dragged)
+    //     // .on("end", dragended));
+
+    //     // const simulation = d3.forceSimulation(nodes)
+    //     // .force("link", d3.forceLink(links).id(d => d.id))
+    //     // .force("charge", d3.forceManyBody().strength(-200))
+    //     // .force("center", d3.forceCenter(width / 2, height / 2))
+    //     // .force("collide", d3.forceCollide(100))
+    //     // .on("tick", ticked);
+
+
+    //     // function ticked() {
+    //     //     // Allow user to move around nodes wherever they want 
+    //     //     // simulation
+    //     //     // .force("center", d3.forceCenter().strength(0))
+
+    //     //     link
+    //     //         .attr("x1", d => d.source.x)
+    //     //         .attr("y1", d => d.source.y)
+    //     //         .attr("x2", d => d.target.x)
+    //     //         .attr("y2", d => d.target.y);
+        
+    //     //     // nodeTextGroup
+    //     //     // .attr("transform", d => 'translate("+[d.x,d.y]+")');
+
+    //     //     node
+    //     //     .attr("cx", d => d.x)
+    //     //     .attr("cy", d => d.y);
+
+    //     //     nodeText
+    //     //     .attr("x", d => d.x)
+    //     //     .attr("y", d => d.y);
+    //     // };
+
+    //     // function dragstarted(event) {
+    //     //     if (!event.active) simulation.alphaTarget(0.1).restart();
+    //     //     event.fx = event.subject.x;
+    //     //     event.fy = event.subject.y;
+    //     // };
+        
+    //     // // // Update the subject (dragged node) position during drag.
+    //     // function dragged(event) {
+    //     //     event.subject.fx = event.x;
+    //     //     event.subject.fy = event.y;
+    //     // };
     
-        // Initial zoom level (optional)
-        svg.call(zoomHandler.transform, d3.zoomIdentity.scale(1));
+    //     // // // Restore the target alpha so the simulation cools after dragging ends.
+    //     // // // Unfix the subject position now that it’s no longer being dragged.
+    //     // function dragended(event) {
+    //     //     if (!event.active) simulation.alphaTarget(0);
+    //     //     event.subject.fx = null;
+    //     //     event.subject.fy = null;
+    //     // };
+
+    //     // simulation.alpha(0.05).restart();
+
+    //     // const zoomHandler = d3.zoom().on('zoom', (event) => {
+    //     //     // Apply transformations to the entire graph (nodes, links, etc.)
+    //     //     nodeTextGroup.attr('transform', event.transform);
+    //     //     link.attr('transform', event.transform);
+    //     //   });
+        
+    //     // // Apply the zoom behavior to the SVG
+    //     // svg.call(zoomHandler);
+    
+    //     // // Initial zoom level (optional)
+    //     // svg.call(zoomHandler.transform, d3.zoomIdentity.scale(1));
 
         
-    }, [data]);
+    // }, [data]);
 
     function addCurrentWordToGraph(word) {
         const lastLetter = word.slice(-1);
